65bf235b3550bfc4412e3d0347804443ae70a8e9
JacielCastro <156726641+JacielCastro@users.noreply.github.com>
2024-04-08 11:51:40 -0300

    Add files via upload
    08/04/2024

--- comandos_exercicios.sql
+++ comandos_exercicios.sql
@@ -4,23 +4,29 @@
 -- Tudo Certo !!!
 select * from livro
 join SOCIOLOGIA on SOCIOLOGIA.numero_registro = LIVRO.numero_registro;
+
 /*
 2. Listar os nomes dos funcionários que são bibliotecários.
 */
 -- Tudo Certo !!!
 select nome from FUNCIONARIO;
+
 /*
 3. Exibir os títulos dos livros publicados antes de 2000.
 */
+select titulo, ano_publicacao from livro
+where ano_publicodo < '2000';
+
 -- Tudo Certo !!! 
 select titulo from LIVRO
 where ano_publicacao < 2000;
 /*
 4. Mostrar o número total de livros em cada biblioteca.
 */
-select * from LIVRO
-join LIVRO_BIBLIOTECA on LIVRO_BIBLIOTECA.numero_registro = LIVRO_BIBLIOTECA.numero_registro
-and ;
+select cnpj, COUNT(*) as total_livro
+from LIVRO_BIBLIOTECA
+group by cnpj;
+
 /*
 5. Listar os eventos do tipo 'Workshop' que ocorreram após 2020.
 */
@@ -50,17 +56,22 @@ and PERIODICO_BIBLIOTECA  like 'titulo';
 /*
 9. Mostrar os nomes dos funcionários que não são técnicos de TI.
 */
-select nome from FUNCIONARIO 
- not like tecnico_ti;
+-- Certa 
+select * from FUNCIONARIO 
+where matricula not like 't%'; 
+
 /*
 10.Exibir o título e o autor dos livros emprestados pelo usuário com ID 'U0001'
 */
+
 /*
 11.Listar todas as palestras que custaram mais de R$ 100,00.
 */
+
 /*
 12.Retornar os títulos dos livros de ciência que foram publicados após 2010.
 */
+
 /*
 13.Exibir os nomes dos atendentes que têm 'Maria' no nome.
 */
@@ -76,14 +87,55 @@ select nome from FUNCIONARIO
 /*
 17.Retornar os títulos dos periódicos que têm 'Saúde' no título.
 */
+-- Certo
+SELECT PERIODICO.titulo
+FROM PERIODICO
+WHERE PERIODICO.titulo LIKE '%saúde%';
+
 /*
 18.Listar os nomes dos estagiários que começam com a letra 'L'.
 */
+-- Certo
+select ESTAGIARIO.nome
+from ESTAGIARIO
+where ESTAGIARIO.nome like 'l%';
+
 /*
 19.Mostrar os títulos dos livros de sociologia que foram publicados nos anos 90.
 */
+-- Certo 
+select titulo, ano_publicacao
+from LIVRO
+join SOCIOLOGIA on SOCIOLOGIA.numero_registro = LIVRO.numero_registro
+where ano_publicacao >= 1990 and ano_publicacao < 2000;
+
 /*
 20.Exibir os títulos dos livros e os nomes dos autores dos livros emprestados em 
 fevereiro de 2023.
 */
 
+/*
+21. todos os livros que não são de tecnologia 
+*/
+-- Certa 
+select * from LIVRO
+where numero_registro 
+not in (select numero_registro from TECNOLOGIA);
+
+/*
+22. Calcule a media de preço dos eventos do tipo worshop.
+*/
+-- Certo esta faltando
+create view media_preco_workshop
+as 
+select ROUND(avg(custo),2) as media_preco
+from EVENTO
+join 
+
+/*
+23. soma de todos os eventos que ocorreram a partir de 2022.
+*/
+-- Certo 
+select round (sum(custo),2) as soma_preco
+from EVENTO
+where YEAR (data) >= 2022;
